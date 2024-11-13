b4c829995942d7c18e8d8a2062fa1de074f4dcf1
ghifarij <afga.ghifari@gmail.com>
2024-11-13 15:23:14 +0700

    Fix text size

--- src/components/aboutDesc.tsx
+++ src/components/aboutDesc.tsx
@@ -1,12 +1,12 @@
 export default function AboutDesc() {
   return (
-    <div className="w-full md:w-3/5 px-4 flex flex-col">
+    <div className="w-full md:w-3/5 px-6 md:px-4 flex flex-col">
       <h1 className="font-bold text-4xl font-serif my-2">Tentang Kami</h1>
       <div className="md:mr-32">
-        <h2 className="font-semibold mt-5 mb-1 font-serif">
+        <h2 className="font-semibold mt-5 mb-1 font-serif text-sm md:text-base">
           Menghadirkan Keaslian Kopi Nusantara ke Setiap Cangkir
         </h2>
-        <p className="text-base leading-relaxed mb-4">
+        <p className="text-sm md:text-base leading-relaxed mb-4">
           Didirikan dengan semangat untuk memperkenalkan kekayaan rasa kopi
           Nusantara kepada seluruh lapisan masyarakat, perusahaan kami lahir
           dari kecintaan mendalam terhadap biji kopi berkualitas. Berawal dari
@@ -16,10 +16,10 @@ export default function AboutDesc() {
         </p>
       </div>
       <div className="md:mr-32">
-        <h2 className="font-semibold mt-5 mb-1 font-serif">
+        <h2 className="font-semibold mt-5 mb-1 font-serif text-sm md:text-base">
           Kolaborasi dan Ketulusan: Nilai di Balik Setiap Butir Kopi
         </h2>
-        <p className="text-base leading-relaxed mb-4">
+        <p className="text-sm md:text-base leading-relaxed mb-4">
           Kami berpegang teguh pada budaya saling menghargai, transparansi, dan
           inovasi. Kami percaya bahwa setiap orang dalam tim kami adalah bagian
           dari perjalanan biji kopi ini, sehingga kami mendorong budaya kerja
